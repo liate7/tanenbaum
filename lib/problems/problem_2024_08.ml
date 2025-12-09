85a6bf2dcb0cdfd53a684bb1e8ec12f2920c1bf9
Andrew Patterson <andrewpatt7@gmail.com>
2025-12-08 23:07:55 -0500

    Before start 2025

---
+++ lib/problems/problem_2024_08.ml
@@ -0,0 +1,26 @@
+open! Common
+
+let year = 2024
+let day = 8
+
+let example =
+  "............\n\
+   ........0...\n\
+   .....0......\n\
+   .......0....\n\
+   ....0.......\n\
+   ......A.....\n\
+   ............\n\
+   ............\n\
+   ........A...\n\
+   .........A..\n\
+   ............\n\
+   ............\n"
+
+module Part_1 = struct
+  let run (input : string) : (string, string) result = Ok input
+end
+
+module Part_2 = struct
+  let run (input : string) : (string, string) result = Ok input
+end
