9cbc98b7c863b46fe74ee36a289286a4d2f01db2
Andrew Patterson <andrewpatt7@gmail.com>
2025-12-11 18:48:02 -0500

    2025, day 4, part 2

--- lib/problems/problem_2025_04.ml
+++ lib/problems/problem_2025_04.ml
@@ -39,7 +39,10 @@ let example =
    x.x.@@@.x. *)
 
 module Grid = struct
-  type t = { dim : int * int; data : [ `Roll | `Empty ] Array.t Array.t }
+  type 'a t = {
+    dim : int * int;
+    data : ([> `Roll | `Empty ] as 'a) Array.t Array.t;
+  }
 
   let of_string str =
     let f =
@@ -53,7 +56,7 @@ module Grid = struct
     let data =
       str |> String.trim |> String.lines_iter |> Iter.map ~f |> Iter.to_array
     in
-    { data; dim = (Array.length @@ Array.get data 0, Array.length data) }
+    { data; dim = (Array.length data, Array.length @@ Array.get data 0) }
 
   let to_string { data; dim = _ } =
     Iter.(
@@ -63,11 +66,11 @@ module Grid = struct
              |> String.of_array)
       |> String.concat_iter ~sep:"\n")
 
-  let in_bounds { dim = width, height; data = _ } (x, y) =
+  let in_bounds { dim = height, width; data = _ } (y, x) =
     0 <= x && x < width && 0 <= y && y < height
 
-  let get (x, y) ({ data; dim = _ } as t) =
-    if in_bounds t (x, y) then Some data.(x).(y) else None
+  let get (y, x) ({ data; dim = _ } as t) =
+    if in_bounds t (y, x) then Some data.(y).(x) else None
 
   let ( .%() ) t at = get at t |> Option.get_exn_or "Invalid index"
 
@@ -77,7 +80,7 @@ module Grid = struct
     Monoid.
       { identity = Map'.empty; reduce = Map'.union (fun _ l r -> Some (l + r)) }
 
-  let neighbors (x, y) =
+  let neighbors (y, x) =
     let id = Fun.id and incr x = x + 1 and decr x = x - 1 in
     [
       (id, incr);
@@ -89,7 +92,7 @@ module Grid = struct
       (decr, id);
       (decr, decr);
     ]
-    |> List.map ~f:(fun (f, g) -> (f x, g y))
+    |> List.map ~f:(fun (f, g) -> (f y, g x))
 
   let is_roll at t = match t.%(at) with `Roll -> true | `Empty -> false
 
@@ -98,14 +101,21 @@ module Grid = struct
       neighbors at |> of_list
       |> filter ~f:(in_bounds t)
       |> filter ~f:(Fun.flip is_roll t)
-      |> length |> Map'.singleton at)
+      |> length |> Pair.make at)
 
-  let adjacency ({ dim = width, height; data = _ } as t) =
+  let adjacency ({ dim = height, width; data = _ } as t) =
     Iter.(
-      product (0 --^ width) (0 --^ height)
+      product (0 --^ height) (0 --^ width)
       |> filter ~f:(fun at -> is_roll at t)
-      |> map ~f:(adjacencies t)
-      |> reduce ~m:map_monoid)
+      |> map ~f:(adjacencies t))
+
+  let ( .%()<- ) { data; dim = _ } (y, x) value = data.(y).(x) <- value
+  let copy t = { t with data = t.data |> Array.map ~f:Array.copy }
+
+  let remove t at =
+    let ret = copy t in
+    ret.%(at) <- `Empty;
+    ret
 end
 
 let parse = Grid.of_string
@@ -116,7 +126,7 @@ let mark_taken grid =
     grid.Grid.data
     |> Array.map ~f:(Array.map ~f:(function `Roll -> '@' | `Empty -> '.'))
   in
-  grid |> Grid.adjacency
+  grid |> Grid.adjacency |> Grid.Map'.of_iter
   |> Grid.Map'.filter (fun _ adj -> adj < 4)
   |> Grid.Map'.keys
   |> Iter.iter ~f:(fun (y, x) ->
@@ -128,23 +138,59 @@ let mark_taken grid =
 
 module Part_1 = struct
   let go grid =
-    Iter.(
-      grid |> Grid.adjacency |> Grid.Map'.to_iter
-      |> filter ~f:(fun (_, adj) -> adj < 4)
-      |> length)
+    grid |> Grid.adjacency |> Grid.Map'.of_iter
+    |> Grid.Map'.filter (fun _ adj -> adj < 4)
+    |> Grid.Map'.cardinal
 
   let run (input : string) : (string, string) result =
     Result.guard_str @@ fun () -> parse input |> go |> unparse
 end
 
-(* Problem description update *)
+(* Once a roll of paper can be accessed by a forklift, it can be removed. Once a roll of paper
+   is removed, the forklifts might be able to access more rolls of paper, which they might also
+   be able to remove. How many total rolls of paper could the Elves remove if they keep
+   repeating this process? *)
 
-(* Example run-through, again *)
+(* Starting with the same example as above, here is one way you could remove as many rolls of
+   paper as possible, using highlighted @ to indicate that a roll of paper is about to be
+   removed, and using x to indicate that a roll of paper was just removed: *)
 
 module Part_2 = struct
-  let parse = Fun.id
-  let unparse = Fun.id
-  let go = Fun.id
+  let visualize grid =
+    let rec go acc grid =
+      let moveable =
+        Iter.(
+          grid |> Grid.adjacency
+          |> filter ~f:(fun (_, adj) -> adj < 4)
+          |> map ~f:fst |> to_list)
+      in
+      match moveable with
+      | [] -> acc
+      | moveable ->
+          Printf.printf "Remove %d rolls of paper:\n%s\n\n"
+            (List.length moveable)
+          @@ mark_taken grid;
+          let grid = List.fold_left ~f:Grid.remove ~init:grid moveable in
+          go (acc + List.length moveable) grid
+    in
+    Printf.printf "Initial state:\n%s\n\n" @@ Grid.to_string grid;
+    go 0 grid
+
+  let go grid =
+    let rec go acc grid =
+      let moveable =
+        Iter.(
+          grid |> Grid.adjacency
+          |> filter ~f:(fun (_, adj) -> adj < 4)
+          |> map ~f:fst |> to_list)
+      in
+      match moveable with
+      | [] -> acc
+      | moveable ->
+          let grid = List.fold_left ~f:Grid.remove ~init:grid moveable in
+          go (acc + List.length moveable) grid
+    in
+    go 0 grid
 
   let run (input : string) : (string, string) result =
     Result.guard_str @@ fun () -> parse input |> go |> unparse
